72b382e5c7444cb0153b578702552a24841456fc
galipabay <galipabay@GALIPSSTARSHIP>
2026-03-07 23:09:13 +0300

    Drizzle Config Create refac

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./lib/db/schema.ts", // Tablo tanýmlarýnýn bulunacaðý yer
-  out: "./drizzle",            // SQL migration dosyalarýnýn oluþturulacaðý klasör
-  dialect: "mysql",            // Kullanacaðýmýz veritabaný türü
+  schema: "./lib/db/schema.ts",
+  out: "./drizzle",            
+  dialect: "mysql",            
   dbCredentials: {
-    url: process.env.DATABASE_URL!, // .env dosyasýndaki baðlantý adresi
+    url: process.env.DATABASE_URL!, 
   },
 });
