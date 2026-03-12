19d6771e53db46b2aee89a59afeccf01507f3b83
galipabay <galipabay@GALIPSSTARSHIP>
2026-03-12 23:13:10 +0300

    Galip A.

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/request';
 import { jwtVerify } from 'jose';
 
-// API rotalarýndaki anahtarla birebir ayný olduðundan emin ol!
 const JWT_SECRET = new TextEncoder().encode("cok_gizli_ve_uzun_bir_key_123");
 
-export async function middleware(request: NextRequest) { // Ýsmi 'middleware' yaptýk
+export async function middleware(request: NextRequest) { 
     const token = request.cookies.get('session')?.value;
     const { pathname } = request.nextUrl;
 
